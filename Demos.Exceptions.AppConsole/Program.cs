11599e676a8d5f816b4d411f393ed66a782b3703
Legrain Samuel <samuel.legrain@bstorm.be>
2021-05-21 16:09:17 +0200

    Ajoutez des fichiers projet.

---
+++ Demos.Exceptions.AppConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Demos.Exceptions.AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                CommonObjects.ExceptionsFactory.BadArgument();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ Demos.Exceptions.CommonObjects/ExceptionsFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Demos.Exceptions.CommonObjects
+{
+    public static class ExceptionsFactory
+    {
+        public static void BadArgument()
+        {
+            throw new ArgumentException();
+        }
+
+        public static void NotImplemented()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
